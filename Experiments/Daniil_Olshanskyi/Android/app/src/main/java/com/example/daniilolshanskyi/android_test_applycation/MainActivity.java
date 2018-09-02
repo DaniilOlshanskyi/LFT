c09cd83d40a952cbf73d3b5482664e12c688fe69
daniilo <daniilo@iastate.edu>
2018-09-02 18:39:53 -0500

    Added two first buttons to my first Android program

--- Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/MainActivity.java
+++ Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.daniilolshanskyi.android_test_applycation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.buttonMessage);
+        b2 = (Button) findViewById(R.id.buttonNext);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello Android Toast???", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
