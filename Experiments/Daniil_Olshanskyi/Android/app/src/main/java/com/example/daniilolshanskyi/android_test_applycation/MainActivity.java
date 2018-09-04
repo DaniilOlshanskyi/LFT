24ced53ab4a12019b15ed0cd1bbdc1b7c8674f65
daniilo <daniilo@iastate.edu>
2018-09-03 19:15:21 -0500

    Added onCreate and onStart console listeners (my_log_message tag)

--- Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/MainActivity.java
+++ Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/MainActivity.java
@@ -6,15 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "my_log_message";
+
     Button b1, b2;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "OnStart");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
 
         b1 = (Button) findViewById(R.id.buttonMessage);
         b2 = (Button) findViewById(R.id.buttonNext);
