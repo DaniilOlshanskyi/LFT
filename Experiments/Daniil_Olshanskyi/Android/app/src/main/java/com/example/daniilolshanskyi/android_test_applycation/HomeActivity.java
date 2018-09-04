79d34aa04b99cee2786b02dc3968172efde28a33
daniilo <daniilo@iastate.edu>
2018-09-03 19:05:41 -0500

    Back button added to hello activity

--- Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/HomeActivity.java
+++ Experiments/Daniil_Olshanskyi/Android/app/src/main/java/com/example/daniilolshanskyi/android_test_applycation/HomeActivity.java
@@ -1,13 +1,29 @@
 package com.example.daniilolshanskyi.android_test_applycation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        b1 = (Button) findViewById(R.id.buttonBack);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
