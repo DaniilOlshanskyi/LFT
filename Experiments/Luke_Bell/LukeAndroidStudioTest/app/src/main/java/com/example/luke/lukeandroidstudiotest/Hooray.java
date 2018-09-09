ca59f335a1665c4f8f59810e14ea59d29fc718f7
ljbell97 <ljbell97@iastate.edu>
2018-09-09 00:10:50 -0500

    Luke: Android Test - Hello Android App

---
+++ Experiments/Luke_Bell/LukeAndroidStudioTest/app/src/main/java/com/example/luke/lukeandroidstudiotest/Hooray.java
@@ -0,0 +1,15 @@
+package com.example.luke.lukeandroidstudiotest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+
+public class Hooray extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hooray);
+    }
+}
--- Experiments/Luke_Bell/LukeAndroidStudioTest/app/src/main/java/com/example/luke/lukeandroidstudiotest/MainActivity.java
+++ Experiments/Luke_Bell/LukeAndroidStudioTest/app/src/main/java/com/example/luke/lukeandroidstudiotest/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.luke.lukeandroidstudiotest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.HelloAndroidButton);
+        b2 = (Button) findViewById(R.id.ThankGodButton);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello user!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Hooray.class);
+                startActivity(i);
+            }
+        });
     }
 }
