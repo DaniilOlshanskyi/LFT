2f80ece0e6dea167908b1a5c522eac9bc7476779
daniilo <daniilo@iastate.edu>
2018-09-08 21:53:34 -0500

    Redone petclinic, now it can be built

--- Experiments/Daniil_Olshanskyi/petclinic-mysql-master/src/main/java/org/springframework/samples/petclinic/owner/CityRepository.java
+++
@@ -1,6 +0,0 @@
-package org.springframework.samples.petclinic.owner;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CityRepository extends CrudRepository<City, Integer> {
-}
