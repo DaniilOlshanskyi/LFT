606a0d1cfa4cb2c564b125343316ec93eaba76fa
Irfan Rafie <irrafie@iastate.edu>
2018-09-09 15:51:16 -0500

    App Commit

---
+++ Experiments/Rafie_Irfan/Learny/app/src/main/java/edu/iastate/irrafie/learny/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.iastate.irrafie.learny;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.test_button);
+
+    }
+
+
+    public native String stringFromJNI();
+}
