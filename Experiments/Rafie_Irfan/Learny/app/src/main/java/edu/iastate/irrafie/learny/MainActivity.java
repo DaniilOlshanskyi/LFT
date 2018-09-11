2ed62fdf90a9df19bc307792da8d4b5468d10305
Irfan Rafie <irrafie@iastate.edu>
2018-09-11 08:54:45 -0500

    trying onClickListeners

--- Experiments/Rafie_Irfan/Learny/app/src/main/java/edu/iastate/irrafie/learny/MainActivity.java
+++ Experiments/Rafie_Irfan/Learny/app/src/main/java/edu/iastate/irrafie/learny/MainActivity.java
@@ -1,9 +1,11 @@
 package edu.iastate.irrafie.learny;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -14,8 +16,19 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
-    Button login;
-    EditText id, name, email, address, zipcode;
+    private Button send_button;
+    private EditText emailText;
+    private EditText passText;
+
+    //this causes crashes
+    //not due to null textbox
+
+    private View.OnClickListener sendButtonOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            send_buttonClicked();
+        }
+    };
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -24,7 +37,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Test
 
+
+        send_button.setOnClickListener(sendButtonOnClickListener);
     }
+
+    private void send_buttonClicked(){
+        String email = emailText.toString();
+        String pass = passText.toString();
+        emailText.setText("Test");
+        passText.setText("Test");
+    }
+
 
 
 }
