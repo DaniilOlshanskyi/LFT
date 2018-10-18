ad4d495f37889fa94f2a8e0a440f334e5de36989
daniilo <daniilo@iastate.edu>
2018-10-18 14:00:05 -0500

    Added games entitity to Spring

---
+++ Backend/LFT/src/main/java/lft/games/Games.java
@@ -0,0 +1,32 @@
+package lft.games;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="games")
+public class Games implements Serializable{
+	
+	@Column(name="gameId")
+    private @Id @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY ) Integer gameId;
+	
+	@Column(name="gameName")
+    private String gameName;
+	
+	@Column(name="gameLogo")
+    private String gameLogo;
+
+	@Column(name="gameRanks")
+    private String gameRanks;
+	
+	@Column(name="gamePlatform")
+    private String gamePlatform;
+	
+	@Column(name="gameRoles")
+    private String gameRoles;
+}
