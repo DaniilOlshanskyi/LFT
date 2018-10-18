a58dfb08688b805da57c61f180535066f7410b83
daniilo <daniilo@iastate.edu>
2018-10-18 14:03:00 -0500

    Added GamesController

---
+++ Backend/LFT/src/main/java/lft/games/GamesController.java
@@ -0,0 +1,29 @@
+package lft.games;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lft.demo.Profiles;
+import lft.demo.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class GamesController {
+
+	@Autowired
+    private GamesRepository gamesRepository;
+	
+	@GetMapping(path="/all")
+    public @ResponseBody Iterable<Profiles> getAllUsers(){
+    	return gamesRepository.findAll();
+    }
+}
