9b1a65e2dbfc11f7969278dd44166f41a4843697
MikeOstrow <mike.ostrow17@gmail.com>
2018-10-29 13:13:02 -0500

    Report Post Page

--- Frontend/lft_lookingforteam/app/src/main/java/com/example/luke/lft_lookingforteam/net_utils/Const.java
+++ Frontend/lft_lookingforteam/app/src/main/java/com/example/luke/lft_lookingforteam/net_utils/Const.java
@@ -1,7 +1,7 @@
 package com.example.luke.lft_lookingforteam.net_utils;
 
 public class Const {
-    // URLs
+    // profile URLs
     public static final String URL_POST_PROFILE = "http://proj309-ad-08.misc.iastate.edu:8080/post_profile";
     public static final String URL_GET_PROFILE_BY_USERNAME = "http://proj309-ad-08.misc.iastate.edu:8080/all/username/";
 
@@ -16,7 +16,17 @@ public class Const {
     public static final String PROFILE_LATEST_LOGIN_DATE_KEY = "profRecentlogin";
     public static final String PROFILE_SUSPENDED_KEY = "profSuspend";
 
+    //report URLs
+    public static final String URL_POST_REPORT = "http://proj309-ad-08.misc.iastate.edu:8080/post_report";
+
+    //report JSON object key names
+    public static final String REPORT_CHATLOG = "reportChatlog";
+    public static final String REPORT_RESOLVE_FLAG = "reportResolveFlag";
+    public static final String REPORT_RESOLVE_DATE = "reportResolveDate";
+    public static final String REPORT_MESSAGE = "reportMessage";
+
     // EditText field character limits
     public static final int USERNAME_CHARACTER_LIMIT = 15;
     public static final int PASSWORD_CHARACTER_LIMIT = 30;
+    public static final int REPORT_CHARACTER_LIMIT = 1200;
 }
