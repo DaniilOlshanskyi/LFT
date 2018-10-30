da1e61367c7c7118275fc965170f0bb77830b6dd
daniilo <daniilo@iastate.edu>
2018-10-30 17:27:47 -0500

    Trying to debug websockets

--- Backend/LFT/src/main/java/lft/websocket/WebSocketConfig.java
+++ Backend/LFT/src/main/java/lft/websocket/WebSocketConfig.java
@@ -1,5 +1,18 @@
 package lft.websocket;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-}
+/**
+ * 
+ * @author Vamsi Krishna Calpakkam
+ *
+ */
+@Configuration 
+public class WebSocketConfig {  
+    @Bean  
+    public ServerEndpointExporter serverEndpointExporter(){  
+        return new ServerEndpointExporter();  
+    }  
+} 
--- Backend/LFT/src/main/java/lft/websocket/WebSocketServer.java
+++ Backend/LFT/src/main/java/lft/websocket/WebSocketServer.java
@@ -23,7 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@ServerEndpoint("/websocket/{username}/{username2}")
+@ServerEndpoint(value="/websocket/{username}")
 @Component
 public class WebSocketServer {
 	private Session session;
