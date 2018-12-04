4dec7ef2a0e284c3b942a0a575c4e6a9b4e78aad
daniilo <daniilo@iastate.edu>
2018-12-03 19:19:07 -0600

    Added del profile implementation

--- Backend/LFT/src/main/java/lft/demo/HelloController.java
+++ Backend/LFT/src/main/java/lft/demo/HelloController.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -131,5 +132,11 @@ public class HelloController {
     		return e.toString();
     	}
     	return "User " + user.getprofUsername() + " is no longer a moderator!";
+    }
+    
+    @DeleteMapping(path="/del_profile/{id}")
+    public @ResponseBody String delProfile( @PathVariable(value="id") int id){
+    	userRepository.deleteById(id);
+    	return "Deleted user with id = " + id;
     }
 } 
